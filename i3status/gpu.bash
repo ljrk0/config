315e3e1ce9629c2a4af4b2aca8a3bbde730dfbc2
LeonardKoenig <leonard.r.koenig@googlemail.com>
2015-02-24 10:06:32 +0100

    added doc to gpu.bash

--- i3status/gpu.bash
+++ i3status/gpu.bash
@@ -1,9 +1,17 @@
+# read line 3 times; we need infinite JSON array for i3status
 i3status | (read line && echo $line && read line && echo $line && while :
 do
+	# read line to modify
 	read line
-	temp=$(nvidia-smi -q -d TEMPERATURE | grep "GPU Current" | awk '{print $5;}')
+	
+	# temperature
+	#temp=$(nvidia-smi -q -d TEMPERATURE | grep "GPU Current" | awk '{print $5;}')
+	
+	# gpuutilization
 	gpuutilstats=`nvidia-settings -t -q "$gpuid"/GPUUtilization | tr ',' '\n'`
 	gpuusage=`echo "$gpuutilstats"|grep graphics|sed 's/[^0-9]//g'`
 	temp="[{ \"full_text\":\"GPU: ${gpuusage}%\"},"
+	
+	# print
  	echo "${line/[/$temp}" || exit 1
 done)
