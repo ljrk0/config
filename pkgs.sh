5798265cc7396d96f8a3593e4cd0313b59efbda7
Leonard König <leonard.r.koenig@googlemail.com>
2020-02-08 16:07:29 +0100

    install backlightctl, kitty, fix script, don't rebuild yay

--- pkgs.sh
+++ pkgs.sh
@@ -5,26 +5,29 @@ set -e
 # base is not a group
 groups="base-devel kf5 kf5-aids plasma qt qt5 texlive-most texlive-lang"
 
-sway="sddm bemenu brightnessctl dex grim i3status-rust-git j4-dmenu-desktop mako slurp sway swaybg swayidle swaylock waybar kanshi"
-kde="konsole okular dolphin"
-apps="firefox libreoffice-fresh  thunderbird biber texlive-most-doc"
+sway="sddm bemenu brightnessctl dex grim i3status-rust-git j4-dmenu-desktop mako slurp sway swaybg swayidle swaylock waybar kanshi backlightctl"
+kde="okular dolphin"
+apps="firefox libreoffice-fresh  thunderbird biber texlive-most-doc kitty"
 vms="libvirt virt-manager qemu ovmf freerdp"
 terminal="htop kakoune-git vim openssh openbsd-netcat tmux zip unzip tree zsh pacman-contrib"
-theme="qt5ct lxappearance-gtk3 noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-iosevka-ss05 dconf-editor"
+theme="qt5ct lxappearance-gtk3 noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-iosevka-ss05 ttf-font-awesome powerline-fonts dconf-editor"
 boot="sbsigntools sbupdate-git efibootmgr efitools intel-ucode"
 net="dhclient networkmanager"
-audio="pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-zeroconf} pavucontrol-qt"
+audio="pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-zeroconf pavucontrol-qt"
 misc="xdg-desktop-portal xdg-user-dirs reflector rustup"
 
 pkgs="$sway $kde $apps $vms $terminal $theme $boot $net $audio $misc"
 
-pushd /tmp
-curl --output yay.tar.gz https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz
-tar xvf yay.tar.gz
-cd yay
-makepkg -s
-sudo pacman -U yay-*.pkg.tar.xz
-popd
+command -v yay > /dev/null
+if [ $? -eq 1 ]; then
+    pushd /tmp
+    curl --output yay.tar.gz https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz
+    tar xvf yay.tar.gz
+    cd yay
+    makepkg -s
+    sudo pacman -U yay-*.pkg.tar.xz
+    popd
+fi
 
 yay -S --needed $groups
 yay -S --needed $pkgs
