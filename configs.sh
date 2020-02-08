9018b681886aa236d0defd958f608365096a82d9
Leonard König <leonard.r.koenig@googlemail.com>
2020-02-08 15:46:35 +0100

    fix config.sh

--- configs.sh
+++ configs.sh
@@ -4,10 +4,10 @@ set -e
 
 mkdir -p ~/.config
 ln -sf $PWD/environment ~/.config/environment.d
-ln -sf i3status-rs ~/.config/
-ln -sf sway ~/.config/
+ln -sf $PWD/i3status-rs ~/.config/
+ln -sf $PWD/sway ~/.config/
 
-ln -sf zsh/zprofile ~/.zprofile
-ln -sf zsh/zshenv ~/.zshenv
-ln -sf zsh/zshrc ~/.zshrc
-ln -sf sh/profile ~/.profile
+ln -sf $PWD/zsh/zprofile ~/.zprofile
+ln -sf $PWD/zsh/zshenv ~/.zshenv
+ln -sf $PWD/zsh/zshrc ~/.zshrc
+ln -sf $PWD/sh/profile ~/.profile
